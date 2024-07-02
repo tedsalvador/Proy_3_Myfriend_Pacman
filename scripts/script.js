e75d7d168992f3305cbe5bfe894eb3f144c2c69c
tedsalvador <tedpaul@hotmail.com>
2024-07-02 23:25:55 +0200

    actualizado

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,39 @@ document.body.addEventListener('click', function() {
     audio.play();
 }); */
 
-document.getElementById('fantasmito').addEventListener('click', function() {
+/* document.getElementById('fantasmito').addEventListener('click', function() {
     this.src = './images/pacman-icon.png';
     this.alt = 'Pac-Man Icon';
+}); */
+
+
+document.getElementById('fantasmito').addEventListener('click', function() {
+    var fantasmito = this;
+    fantasmito.src = './images/pacman-icon.png';
+    fantasmito.alt = 'Pac-Man Icon';
+
+    setTimeout(function() {
+        fantasmito.style.display = 'none';
+
+        setInterval(function() {
+            var newFantasmito = document.createElement('img');
+            newFantasmito.src = './images/fantasmito.png';
+            newFantasmito.alt = 'Fantasmita';
+            newFantasmito.className = 'fantasmita';
+            newFantasmito.id = 'fantasmito';
+            newFantasmito.style.cursor = 'pointer';
+
+            newFantasmito.addEventListener('click', function() {
+                var clickedFantasmito = this;
+                clickedFantasmito.src = './images/pacman-icon.png';
+                clickedFantasmito.alt = 'Pac-Man Icon';
+
+                setTimeout(function() {
+                    clickedFantasmito.style.display = 'none';
+                }, 2000); // Pac-Man icon disappears after 2 seconds
+            });
+
+            document.querySelector('.container').appendChild(newFantasmito);
+        }, 2000);
+    }, 2000); // Pac-Man icon disappears after 2 seconds
 });
